7b0994c5c9227c68e52bf0ee16963721cf41458a
andbudha <andreibartov@gmail.com>
2025-10-15 09:45:27 +0200

    UserContext created/main layout wrapped with UserProvider/useUser custom hook added

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,13 +1,14 @@
 import { StyleSheet, Text, useColorScheme, View } from 'react-native';
 import { Stack } from 'expo-router';
 import { Colors } from '../constants/Colors';
+import { UserContext, UserProvider } from '../contexts/UserContext';
 
 const RootLayout = () => {
   const colorScheme = useColorScheme();
   const theme = Colors[colorScheme] ?? Colors.light;
 
   return (
-    <>
+    <UserProvider>
       <Stack
         screenOptions={{
           headerStyle: { backgroundColor: theme.navBackground },
@@ -20,7 +21,7 @@ const RootLayout = () => {
         <Stack.Screen name="(auth)" options={{ headerShown: false }} />
         <Stack.Screen name="(dashboard)" options={{ headerShown: false }} />
       </Stack>
-    </>
+    </UserProvider>
   );
 };
 
---
+++ contexts/UserContext.jsx
@@ -0,0 +1,36 @@
+import { createContext, useState } from 'react';
+import { account } from '../lib/appwrite';
+import { ID } from 'react-native-appwrite';
+
+export const UserContext = createContext();
+
+export function UserProvider({ children }) {
+  const [user, setUser] = useState(null);
+
+  async function login(email, password) {
+    try {
+      await account.createEmailPasswordSession(email, password);
+      const response = await account.get();
+      setUser(response);
+    } catch (error) {
+      throw Error(error);
+    }
+  }
+
+  async function register(email, password) {
+    try {
+      await account.create(ID.unique(), email, password);
+      await login(email, password);
+    } catch (error) {
+      throw Error(error);
+    }
+  }
+
+  async function logout() {}
+
+  return (
+    <UserContext.Provider value={{ user, login, logout, register }}>
+      {children}
+    </UserContext.Provider>
+  );
+}
