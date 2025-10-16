a3e704b103cd8b8b2648273535e94fbda91e437e
andbudha <andreibartov@gmail.com>
2025-10-16 10:24:44 +0200

    if logged in initial user values caught

--- contexts/UserContext.jsx
+++ contexts/UserContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useState } from 'react';
+import { createContext, useEffect, useState } from 'react';
 import { account } from '../lib/appwrite';
 import { ID } from 'react-native-appwrite';
 
@@ -6,6 +6,7 @@ export const UserContext = createContext();
 
 export function UserProvider({ children }) {
   const [user, setUser] = useState(null);
+  const [authChecked, setAuthChecked] = useState(false);
 
   async function login(email, password) {
     try {
@@ -26,10 +27,31 @@ export function UserProvider({ children }) {
     }
   }
 
-  async function logout() {}
+  async function logout() {
+    await account.deleteSession('current');
+    setUser(null);
+  }
+
+  async function getInitialUserValue() {
+    try {
+      const response = await account.get();
+      setUser(response);
+    } catch (error) {
+      setUser(null);
+      throw Error(error);
+    } finally {
+      setAuthChecked(true);
+    }
+  }
+
+  useEffect(() => {
+    getInitialUserValue();
+  }, []);
 
   return (
-    <UserContext.Provider value={{ user, login, logout, register }}>
+    <UserContext.Provider
+      value={{ user, authChecked, login, logout, register }}
+    >
       {children}
     </UserContext.Provider>
   );
