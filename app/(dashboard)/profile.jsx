59bae06c7728ce1fad646ff95c81d94536c67bc1
andbudha <andreibartov@gmail.com>
2025-10-16 10:25:55 +0200

    logout btn & logout func-ty added

--- app/(dashboard)/profile.jsx
+++ app/(dashboard)/profile.jsx
@@ -2,17 +2,34 @@ import { StyleSheet, Text, View } from 'react-native';
 import ThemedView from '../../components/ThemedView';
 import ThemedText from '../../components/ThemedText';
 import Spacer from '../../components/Spacer';
+import { useUser } from '../../hooks/useUser';
+import ThemedButton from '../../components/ThemedButton';
 
 const Profile = () => {
+  const { logout } = useUser();
+
+  const { user } = useUser();
+  console.log('user:::', user);
+
   return (
     <ThemedView style={styles.container}>
       <Spacer />
       <ThemedText title style={styles.heading}>
         Your Email
       </ThemedText>
+
+      {user && (
+        <>
+          <Spacer />
+          <ThemedText style={styles.heading}>{user.email}</ThemedText>
+        </>
+      )}
       <Spacer />
       <ThemedText>Time to start reading some books...</ThemedText>
       <Spacer />
+      <ThemedButton onPress={logout}>
+        <ThemedText style={styles.btnText}>Logout</ThemedText>
+      </ThemedButton>
     </ThemedView>
   );
 };
@@ -23,11 +40,14 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: 'center',
-    justifyContent: 'stretch',
+    justifyContent: 'center',
   },
   heading: {
     fontSize: 18,
     fontWeight: 'bold',
     textAlign: 'center',
+  },
+  btnText: {
+    color: '#fff',
   },
 });
