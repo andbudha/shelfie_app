1a255c00449b16584559daaf7a6d48c4aa058153
andbudha <andreibartov@gmail.com>
2025-10-24 11:50:03 +0200

    book-list fetched and displayed

--- app/(dashboard)/books.jsx
+++ app/(dashboard)/books.jsx
@@ -1,15 +1,39 @@
-import { StyleSheet, Text, View } from 'react-native';
-import ThemedView from '../../components/ThemedView';
-import ThemedText from '../../components/ThemedText';
-import Spacer from '../../components/Spacer';
+import { FlatList, Pressable, StyleSheet } from "react-native";
+import { useBooks } from "../../hooks/useBooks";
+import { Colors } from "../../constants/Colors";
+
+import ThemedView from "../../components/ThemedView";
+import ThemedText from "../../components/ThemedText";
+import ThemedCard from "../../components/ThemedCard";
+import Spacer from "../../components/Spacer";
 
 const Books = () => {
+  const { books } = useBooks();
+
   return (
     <ThemedView style={styles.container}>
       <Spacer />
       <ThemedText title style={styles.heading}>
         Your Reading List
       </ThemedText>
+      <FlatList
+        data={books}
+        keyExtractor={(item) => item.$id}
+        contentContainerStyle={styles.list}
+        renderItem={(item) => (
+          <Pressable>
+            <ThemedCard style={styles.card}>
+              <ThemedText style={styles.title}>{item.item.title}</ThemedText>
+              <ThemedText style={styles.author}>
+                Written by {item.item.author}
+              </ThemedText>
+              <ThemedText style={styles.description}>
+                {item.item.description}
+              </ThemedText>
+            </ThemedCard>
+          </Pressable>
+        )}
+      />
     </ThemedView>
   );
 };
@@ -19,12 +43,37 @@ export default Books;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'stretch',
+    alignItems: "center",
+    justifyContent: "stretch",
   },
   heading: {
     fontSize: 18,
-    fontWeight: 'bold',
-    textAlign: 'center',
+    fontWeight: "bold",
+    textAlign: "center",
+  },
+  list: {
+    flexGrow: 1,
+  },
+  card: {
+    width: "90%",
+    marginHorizontal: "5%",
+    marginVertical: 10,
+    padding: 10,
+    paddingLeft: 14,
+    borderLeftColor: Colors.primary,
+    borderLeftWidth: 4,
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: "bold",
+    fontStyle: "italic",
+    marginBottom: 10,
+  },
+  author: {
+    fontSize: 16,
+    fontStyle: "italic",
+  },
+  description: {
+    fontSize: 16,
   },
 });
